d9e948eb8da524819e03090c273a62d6ad0e8121
NiekMSoftware <niek+github@meletstudios.com>
2026-07-26 18:52:53 +0200

    refactor `timer.h`: update function signatures to simplify parameters and improve clarity

--- platform/inc/platform/timer.h
+++ platform/inc/platform/timer.h
@@ -16,14 +16,14 @@ inline u64 ticksToNS(const u64 ticks, const u64 freq) {
     return (ticks / freq) * 1'000'000'000ull
          + (ticks % freq) * 1'000'000'000ull / freq;
 }
-u64 platformTicks(void);
-u64 platformFrequency(void);
+u64 platformTicks();
+u64 platformFrequency();
 }  // namespace shiba
 
 extern "C" {
 shiba::Clock shibaClockInit(void);
-shiba::Timer shibaTimerStart(const shiba::Clock* c);
-void         shibaTimerReset(shiba::Timer* t, const shiba::Clock* c);
+shiba::Timer shibaTimerStart(void);
+void         shibaTimerReset(shiba::Timer* t);
 shiba::u64   shibaTimerElapsedNS (const shiba::Timer* t, const shiba::Clock* c);
 shiba::f64   shibaTimerElapsedSec(const shiba::Timer* t, const shiba::Clock* c);
 }
