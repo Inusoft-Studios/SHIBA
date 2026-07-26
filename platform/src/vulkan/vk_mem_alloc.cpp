3e81abe1277e74f969591e6e6c6e97d1be27af85
NiekMSoftware <niek+github@meletstudios.com>
2026-07-26 21:36:32 +0200

    use relative path for VMA header: simplify include directive in Vulkan backend

--- platform/src/vulkan/vk_mem_alloc.cpp
+++ platform/src/vulkan/vk_mem_alloc.cpp
@@ -5,5 +5,5 @@
 
 // VMA is third-party and won't pass our warnings-as-errors. Silence for this TU only.
 SHIBA_WARN_PUSH_DISABLE_ALL()
-#include "../../../thirdparty/vma/vk_mem_alloc.h"
+#include "vk_mem_alloc.h"
 SHIBA_WARN_POP()
