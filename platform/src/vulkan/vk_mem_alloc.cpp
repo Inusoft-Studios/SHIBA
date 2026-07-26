7ecbc91b8ed4b64b85633c5db0b3a8932e4c1ede
NiekMSoftware <niek+github@meletstudios.com>
2026-07-26 21:27:21 +0200

    integrate VMA library: add Vulkan Memory Allocator (VMA) integration and enable usage in Vulkan backend

---
+++ platform/src/vulkan/vk_mem_alloc.cpp
@@ -0,0 +1,9 @@
+#include <vulkan/vulkan.h>
+#include "platform/compiler.h"
+
+#define VMA_IMPLEMENTATION
+
+// VMA is third-party and won't pass our warnings-as-errors. Silence for this TU only.
+SHIBA_WARN_PUSH_DISABLE_ALL()
+#include "../../../thirdparty/vma/vk_mem_alloc.h"
+SHIBA_WARN_POP()
