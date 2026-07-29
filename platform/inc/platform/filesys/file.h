d9b1dca5052283f303f49de7e28fadcfbb8bb3e5
NiekMSoftware <niek+github@meletstudios.com>
2026-07-29 20:38:48 +0200

    add platform headers: file.h and thread.h

---
+++ platform/inc/platform/filesys/file.h
@@ -0,0 +1,8 @@
+// =============================================================================
+//                        Copyright Inusoft Studios
+// =============================================================================
+
+#ifndef SHIBA_FILE_SYS_FILE_H_
+#define SHIBA_FILE_SYS_FILE_H_
+
+#endif  // SHIBA_FILE_SYS_FILE_H_
---
+++ platform/inc/platform/threading/thread.h
@@ -0,0 +1,8 @@
+// =============================================================================
+//                        Copyright Inusoft Studios
+// =============================================================================
+
+#ifndef SHIBA_THREADING_THREAD_H_
+#define SHIBA_THREADING_THREAD_H_
+
+#endif  // SHIBA_THREADING_THREAD_H_
