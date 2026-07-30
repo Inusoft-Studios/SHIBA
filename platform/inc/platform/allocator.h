1267364e7086555acfced05cff7d2f93f257ce9b
NiekMSoftware <niek+github@meletstudios.com>
2026-07-30 15:43:41 +0200

    add inline utility functions to `allocator.h` for memory allocation and alignment

--- platform/inc/platform/allocator.h
+++ platform/inc/platform/allocator.h
@@ -12,6 +12,18 @@ struct AllocationCallbacks {
     void  (*pFnFree)(void* user, void* ptr, usize size, usize align);
     void* pUser;
 };
+
+inline void* alloc(const usize size, const usize align, const AllocationCallbacks* alloc) {
+    return alloc->pFnAlloc(alloc->pUser, size, align);
+}
+
+inline void free(void* ptr, const usize size, const usize align, const AllocationCallbacks* alloc) {
+    alloc->pFnFree(alloc->pUser, ptr, size, align);
+}
+
+inline usize alignUp(const usize v, const usize a) { return (v + a - 1) & ~(a - 1); }
+inline usize maxAlign(const usize a, const usize b) { return a > b ? a : b; }
+
 }  // namespace shiba
 
 #endif  // SHIBA_PLATFORM_ALLOCATOR_H_
