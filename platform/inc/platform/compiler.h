4f1765f4cc8611e2e5c206618c5417f3619b4206
NiekMSoftware <niek+github@meletstudios.com>
2026-07-30 14:25:32 +0200

    add descriptive comments to `compiler.h` for improved code readability

--- platform/inc/platform/compiler.h
+++ platform/inc/platform/compiler.h
@@ -8,13 +8,13 @@
 // === Compiler detection ===
 #if defined (__clang__)
     #define SHIBA_COMPILER_CLANG 1
-#elif defined(_MSC_VER)
+#elif defined(_MSC_VER)     // ^^^ CLANG / vvv MSVC
     #define SHIBA_COMPILER_MSVC  1
-#elif defined(__GNUC__)
+#elif defined(__GNUC__)     // ^^^ MSVC / vvv GCC
     #define SHIBA_COMPILER_GCC   1
-#else
+#else                       // ^^^ GCC / vvv unsupported
     #error "shiba: unsupported compiler"
-#endif
+#endif                      // ^^^ unsupported
 
 // Define the rest to 0 so `#if SHIBA_COMPILER_X` is always valid
 #ifndef SHIBA_COMPILER_CLANG
@@ -30,39 +30,39 @@
 // Configuration detection
 #if NDEBUG
     #define SHIBA_DEBUG 0
-#else
+#else                       // ^^^ NDEBUG / vvv debug
     #define SHIBA_DEBUG 1
-#endif
+#endif                      // ^^^ debug
 
 // === Inlining ===
 #if SHIBA_COMPILER_MSVC
     #define SHIBA_FORCE_INLINE __forceinline
     #define SHIBA_NOINLINE     __declspec(noinline)
-#else
+#else                       // ^^^ MSVC / vvv non-MSVC
     #define SHIBA_FORCE_INLINE inline __attribute__((always_inline))
     #define SHIBA_NOINLINE     __attribute__((noinline))
-#endif
+#endif                      // ^^^ non-MSVC
 
 // === Branch hints ===
 #if SHIBA_COMPILER_MSVC
     // MSVC doesn't know about [[likely]] in C++17
     #define SHIBA_LIKELY(x)   (x)
     #define SHIBA_UNLIKELY(x) (x)
-#else
+#else                       // ^^^ MSVC / vvv non-MSVC
     #define SHIBA_LIKELY(x)   __builtin_expect(!!(x), 1)
     #define SHIBA_UNLIKELY(x) __builtin_expect(!!(x), 0)
-#endif
+#endif                      // ^^^ non-MSVC
 
 // === Control-flow intrinsics ===
 #if SHIBA_COMPILER_MSVC
     #define SHIBA_UNREACHABLE   __assume(0)
     #define SHIBA_DEBUGBREAK()  __debugbreak()
     #define SHIBA_FUNCSIG       __FUNCSIG__
-#else
+#else                       // ^^^ MSVC / vvv non-MSVC
     #define SHIBA_UNREACHABLE   __builtin_unreachable()
     #define SHIBA_DEBUGBREAK()  __builtin_trap()
     #define SHIBA_FUNCSIG       __PRETTY_FUNCTION__
-#endif
+#endif                      // ^^^ non-MSVC
 
 // === Diagnostic push/pop ===
 // compiler.h
@@ -70,19 +70,19 @@
     #define SHIBA_PRAGMA(x)   __pragma(x)
     #define SHIBA_WARN_PUSH_DISABLE_ALL()  SHIBA_PRAGMA(warning(push, 0))
     #define SHIBA_WARN_POP()               SHIBA_PRAGMA(warning(pop))
-#elif SHIBA_COMPILER_CLANG
+#elif SHIBA_COMPILER_CLANG  // ^^^ MSVC / vvv CLANG
     #define SHIBA_PRAGMA(x)   _Pragma(#x)
     #define SHIBA_WARN_PUSH_DISABLE_ALL()             \
         SHIBA_PRAGMA(clang diagnostic push)           \
         SHIBA_PRAGMA(clang diagnostic ignored "-Weverything")
     #define SHIBA_WARN_POP()  SHIBA_PRAGMA(clang diagnostic pop)
-#elif SHIBA_COMPILER_GCC
+#elif SHIBA_COMPILER_GCC    // ^^^ CLANG / vvv GCC
     #define SHIBA_PRAGMA(x)   _Pragma(#x)
     #define SHIBA_WARN_PUSH_DISABLE_ALL()             \
         SHIBA_PRAGMA(GCC diagnostic push)             \
         SHIBA_PRAGMA(GCC diagnostic ignored "-Wall")  \
         SHIBA_PRAGMA(GCC diagnostic ignored "-Wextra")
     #define SHIBA_WARN_POP()  SHIBA_PRAGMA(GCC diagnostic pop)
-#endif
+#endif                      // ^^^ GCC
 
 #endif // SHIBA_PLATFORM_COMPILER_H_
