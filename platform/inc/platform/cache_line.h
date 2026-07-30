67a8c9133de869a5e4c9f1608f76bd154c678e7b
NiekMSoftware <niek+github@meletstudios.com>
2026-07-30 14:29:49 +0200

    add descriptive comments to `cache_line.h` to clarify cache line size definitions

--- platform/inc/platform/cache_line.h
+++ platform/inc/platform/cache_line.h
@@ -7,16 +7,15 @@
 
 #if defined(__APPLE__) && defined(__aarch64__)
     #define SHIBA_CACHE_LINE_SIZE 128   // Apple Silicon
-#elif defined(__powerpc64__) || defined(__ppc64__)
+#elif defined(__powerpc64__) || defined(__ppc64__)      // ^^^ __APPLE__ / vvv __ppc64__
     #define SHIBA_CACHE_LINE_SIZE 128
 #elif defined(__x86_64__) || defined(_M_X64) || \
       defined(__i386__)   || defined(_M_IX86) || \
       defined(__aarch64__) || defined(_M_ARM64) || \
-      defined(__riscv)
+      defined(__riscv)                                  // ^^^ __ppc64__ / vvv mainstream
             #define SHIBA_CACHE_LINE_SIZE 64
-#else
+#else                                                   // ^^^ mainstream / vvv default
     #define SHIBA_CACHE_LINE_SIZE 64    // safe default
-#endif
-
+#endif                                                  // ^^^ default
 
 #endif  // SHIBA_PLATFORM_CACHE_LINE_H_
