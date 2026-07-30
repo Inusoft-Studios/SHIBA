73a9e8f5fd3a1673298dd6045a925d0c8016403e
NiekMSoftware <niek+github@meletstudios.com>
2026-07-30 15:46:44 +0200

    add `shiba` prefix to static_assert messages in `FixedArray` for error clarity

--- platform/inc/platform/collections/dynamic_array.hpp
+++ platform/inc/platform/collections/dynamic_array.hpp
@@ -4,5 +4,15 @@
 
 #ifndef SHIBA_PLATFORM_COLLECTIONS_DYNAMIC_ARRAY_HPP_
 #define SHIBA_PLATFORM_COLLECTIONS_DYNAMIC_ARRAY_HPP_
+#include <type_traits>
+
+namespace shiba {
+
+template<class T>
+struct DynamicArray {
+    static_assert(std::is_trivially_copyable_v<T>, "shiba: T must be trivially copyable");
+};
+
+}  // namespace shiba
 
 #endif  // SHIBA_PLATFORM_COLLECTIONS_DYNAMIC_ARRAY_HPP_
