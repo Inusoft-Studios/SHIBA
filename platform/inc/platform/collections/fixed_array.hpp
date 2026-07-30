ea71ca3fa04ac84000f694bba5de95247168f8d2
NiekMSoftware <niek+github@meletstudios.com>
2026-07-30 11:13:38 +0200

    add `FixedArraySizeType` alias to `fixed_array.hpp` for capacity-based size type selection

--- platform/inc/platform/collections/fixed_array.hpp
+++ platform/inc/platform/collections/fixed_array.hpp
@@ -5,4 +5,17 @@
 #ifndef SHIBA_PLATFORM_COLLECTIONS_FIXED_ARRAY_HPP_
 #define SHIBA_PLATFORM_COLLECTIONS_FIXED_ARRAY_HPP_
 
+#include "platform/cache_line.h"
+#include "platform/types.h"
+
+namespace shiba {
+
+template<usize Capacity>
+using FixedArraySizeType = std::conditional_t<
+    (Capacity <= 0xFFull), u8,
+    std::conditional_t<(Capacity <= 0xFFFFull), u16,
+    std::conditional_t<(Capacity <= 0xFFFFFFFFull), u32, u64>>>;
+
+}  // namespace shiba
+
 #endif  // SHIBA_PLATFORM_COLLECTIONS_FIXED_ARRAY_HPP_
