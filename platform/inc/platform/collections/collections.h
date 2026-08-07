0288adcfcc8e5366e8b072a9fa2683b055ac7db5
NiekMSoftware <niek+github@meletstudios.com>
2026-08-07 12:52:12 +0200

    add `collections.h` umbrella header for common collection utilities

---
+++ platform/inc/platform/collections/collections.h
@@ -0,0 +1,11 @@
+#ifndef SHIBA_PLATFORM_COLLECTIONS_H_
+#define SHIBA_PLATFORM_COLLECTIONS_H_
+
+#include "dynamic_array.hpp"
+#include "fixed_array.hpp"
+#include "hashmap.hpp"
+#include "linked_list.hpp"
+#include "ring_buffer.hpp"
+#include "span.hpp"
+
+#endif  // SHIBA_PLATFORM_COLLECTIONS_H_
